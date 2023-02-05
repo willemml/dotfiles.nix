18fdb1b562202511b993913254a2666a6653c03c
willemml <willem@leit.so>
2023-02-05 14:57:36 -0800

    darwin config key remapping

--- system/darwin.nix
+++ system/darwin.nix
@@ -6,4 +6,25 @@
   nix.package = pkgs.nix;
 
   programs.zsh.enable = true;
+
+  system = {
+    defaults = {
+      loginwindow.SHOWFULLNAME = false;
+      loginwindow.GuestEnabled = false;
+      loginwindow.DisableConsoleAccess = true;
+      finder._FXShowPosixPathInTitle = true;
+      LaunchServices.LSQuarantine = false;
+    };
+    keyboard = {
+    enableKeyMapping = true;
+
+    remapCapsLockToEscape = true;
+
+    # see https://developer.apple.com/library/content/technotes/tn2450/_index.html for more info
+    userKeyMapping = [ {
+      HIDKeyboardModifierMappingSrc = 30064771303; # remap right command to right control.
+      HIDKeyboardModifierMappingDst = 30064771300; 
+    }];
+    };
+  };
 }
