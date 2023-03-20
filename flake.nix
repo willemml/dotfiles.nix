027d0069567f7a0bf4f83415d3c9abffa3af8434
willemml <willem@leit.so>
2023-03-20 14:58:28 -0700

    re-enable other system types

--- flake.nix
+++ flake.nix
@@ -24,10 +24,10 @@
       ];
 
       systems = [
-        # "x86_64-linux"
-        # "x86_64-darwin"
+        "x86_64-linux"
+        "x86_64-darwin"
         "aarch64-darwin"
-        # "aarch64-linux"
+        "aarch64-linux"
       ];
 
       perSystem = {
