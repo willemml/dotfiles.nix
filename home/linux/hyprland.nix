821e873d1f67155f93fd8533e108b5a5850f04ef
willemml <willem@leit.so>
2023-11-16 10:36:27 -0800

    Fixes Hyprland config (settings -> extraConfig)

--- home/linux/hyprland.nix
+++ home/linux/hyprland.nix
@@ -2,13 +2,9 @@
   config,
   pkgs,
   lib,
-  inputs,
+  globals,
   ...
 }: {
-  imports = [
-    inputs.hyprland.homeManagerModules.default
-  ];
-
   home.packages = with pkgs; [
     firefox
     lxappearance
@@ -24,37 +20,30 @@
   services.mako.enable = true;
 
   wayland.windowManager.hyprland = {
-    settings = {
-      input = {
-        sensitivity = 0.4;
-        accel_profile = "adaptive";
-        touchpad = {
-          natural_scroll = true;
-          scroll_factor = 0.45;
-          tap-and-drag = true;
-        };
-      };
+    extraConfig = ''
+      bind = SHIFT_SUPER, SPACE, exec, alacritty
+      bind = SUPER, SPACE, exec, rofi -modes "ssh,drun,window" -show drun
+      bind = ALT, SPACE, exec, rofi -show window
 
-      gestures = {
-        workspace_swipe = true;
-        workspace_swipe_fingers = 3;
-        workspace_swipe_cancel_ratio = 0.5;
-        workspace_swipe_distance = 2000;
-        workspace_swipe_invert = true;
-      };
-    };
+      input {
+        kb_layout=${globals.keyboard.layout}
+        kb_variant=${globals.keyboard.variant}
+        sensitivity=0.4
+        accel_profile=adaptive
+        touchpad {
+          natural_scroll=true
+          scroll_factor=0.45
+          tap-and-drag=true
+        }
+      }
 
-    enable = true;
+      gestures {
+        workspace_swipe=true
+        workspace_swipe_fingers=3
+        workspace_swipe_cancel_ratio=0.5
+        workspace_swipe_distance=2000
+        workspace_swipe_invert=true
+      }
+    '';
   };
-
-  wayland.windowManager.hyprland.extraConfig = ''
-    bind = SHIFT_SUPER, SPACE, exec, alacritty
-    bind = SUPER, SPACE, exec, rofi -modes "ssh,drun,window" -show drun
-    bind = ALT, SPACE, exec, rofi -show window
-
-    input {
-      kb_layout=us
-      kb_variant=colemak
-    }
-  '';
 }
