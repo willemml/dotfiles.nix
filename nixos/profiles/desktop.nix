c001c07c44d223a466a6cc9c39c6335ebda29786
willemml <willem@leit.so>
2024-02-15 00:42:02 -0800

    Enables usbip

--- nixos/profiles/desktop.nix
+++ nixos/profiles/desktop.nix
@@ -9,6 +9,8 @@
     ../users/willem/home/linux.nix
     ../modules/zerotier.nix
   ];
+  boot.kernelModules = ["vhci-hcd" "usbip-core" "usbip-vudc" "usbip-host"];
+  environment.systemPackages = [pkgs.linuxPackages.usbip];
   powerManagement.cpuFreqGovernor = "performance";
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
