a3a0fde5b7fd0edf0e24412aa46920f6669ae55f
willemml <willem@leit.so>
2024-03-11 10:50:57 -0700

    Fixes typo in polkit.nix

--- nixos/modules/polkit.nix
+++ nixos/modules/polkit.nix
@@ -18,7 +18,7 @@
       };
     };
   };
-  nvironment.sessionVariables = {
+  environment.sessionVariables = {
     POLKIT_AUTH_AGENT = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
   };
 }
